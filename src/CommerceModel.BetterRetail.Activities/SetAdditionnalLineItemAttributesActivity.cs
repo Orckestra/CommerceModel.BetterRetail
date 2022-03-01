019f93d7a486c3569b816ed516bf0f02a88944f0
jcmascolo-orckestra <58448993+jcmascolo-orckestra@users.noreply.github.com>
2022-03-01 15:48:00 -0500

    Features/57479 add custom attributes to product data (#58)
    * Add LineItemTest attributes, corresponding Product custom attributes and add them via SetAdditionalLineItemAttributesActivity. Also, adjust Product configs
    
    * Refactoring, corrections and renaming
    
    * Refactoring
    
    * Review after integration

--- src/CommerceModel.BetterRetail.Activities/SetAdditionnalLineItemAttributesActivity.cs
+++ src/CommerceModel.BetterRetail.Activities/SetAdditionnalLineItemAttributesActivity.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 using Orckestra.Overture.Orders.Processing.Workflows.Activities;
+using Orckestra.Overture.ServiceModel.Orders;
+using Orckestra.Overture.ServiceModel.Products;
 
 namespace CommerceModel.BetterRetail.Activities
 {
@@ -28,6 +30,41 @@ namespace CommerceModel.BetterRetail.Activities
         /// The message id for cultureName not found.
         /// </summary>
         public const string CultureNotFoundMessageId = "CultureNotFoundMessage";
+
+        /// <summary>
+        /// The TestBoolean property bag key
+        /// </summary>
+        public const string TestBoolean = nameof(TestBoolean);
+
+        /// <summary>
+        /// The TestText property bag key
+        /// </summary>
+        public const string TestText = nameof(TestText);
+
+        /// <summary>
+        /// The TestLookup property bag key
+        /// </summary>
+        public const string TestLookup = nameof(TestLookup);
+
+        /// <summary>
+        /// The TestMultiLookup property bag key
+        /// </summary>
+        public const string TestMultiLookup = nameof(TestMultiLookup);
+
+        /// <summary>
+        /// The TestDecimal property bag key
+        /// </summary>
+        public const string TestDecimal = nameof(TestDecimal);
+
+        /// <summary>
+        /// The TestInteger property bag key
+        /// </summary>
+        public const string TestInteger = nameof(TestInteger);
+
+        /// <summary>
+        /// The TestDatetime property bag key
+        /// </summary>
+        public const string TestDatetime = nameof(TestDatetime);
 
         /// <summary>
         ///     Loads the product details in the line items of the cart.
@@ -65,23 +102,67 @@ namespace CommerceModel.BetterRetail.Activities
                 lineItem.ProductSummary.AllowSelectionWithoutScan = allowSelectionWithoutScan;
 
                 if (!string.IsNullOrWhiteSpace(imageUrl))
+                {
                     lineItem.PropertyBag[ImageUrlKey] = imageUrl;
+                }
                 else
                 {
-                                    context.ProcessingRecordTracker.TrackError(
-    imageUrl ?? CultureNotFoundMessageId,
-    "The cultureName of the cart was not provided in context.CurrentOrder.Cart.CultureName. Product information will not be retrieved.    " + imageUrl + "-----",
-    new Dictionary<string, object> { { "CartId", context.CurrentOrder.Cart.Id } });
-
+                    context.ProcessingRecordTracker.TrackError(imageUrl ?? CultureNotFoundMessageId,
+                        "The cultureName of the cart was not provided in context.CurrentOrder.Cart.CultureName. Product information will not be retrieved.    " + imageUrl + "-----",
+                        new Dictionary<string, object> { { "CartId", context.CurrentOrder.Cart.Id } });
 
                     lineItem.PropertyBag.Remove(ImageUrlKey);
                 }
+
+                AddPropertyBagForType(lineItem, product, nameof(Boolean), TestBoolean);
+                AddPropertyBagForType(lineItem, product, nameof(String), TestText);
+                AddPropertyBagForType(lineItem, product, nameof(String), TestLookup);
+                AddPropertyBagForType(lineItem, product, nameof(String), TestMultiLookup);
+                AddPropertyBagForType(lineItem, product, nameof(Decimal), TestDecimal);
+                AddPropertyBagForType(lineItem, product, nameof(Int32), TestInteger);
+                AddPropertyBagForType(lineItem, product, nameof(DateTime), TestDatetime);
             }
 
             return Task.CompletedTask;
         }
 
+        private void AddPropertyBagForType(LineItem lineItem,
+            Product product,
+            string type,
+            string key)
+        {
+            switch (type)
+            {
+                case nameof(String):
+                    string stringValue = product?.PropertyBag?.GetOrDefault(key, string.Empty);
+                    AddValueWhenDefined(lineItem, key, stringValue);
+                    break;
+                case nameof(Boolean):
+                    bool? booleanValue = product?.PropertyBag?.GetOrDefault(TestBoolean, false);
+                    AddValueWhenDefined(lineItem, key, booleanValue);
+                    break;
+                case nameof(Decimal):
+                    decimal? decimalValue = product?.PropertyBag?.GetOrDefault(TestDecimal, new decimal());
+                    AddValueWhenDefined(lineItem, key, decimalValue);
+                    break;
+                case nameof(Int32):
+                    int? integerValue = product?.PropertyBag?.GetOrDefault(TestInteger, new int());
+                    AddValueWhenDefined(lineItem, key, integerValue);
+                    break;
+                case nameof(DateTime):
+                    DateTime? dateTimeValue = product?.PropertyBag?.GetOrDefault(TestDatetime, new DateTime());
+                    AddValueWhenDefined(lineItem, key, dateTimeValue);
+                    break;
+                default:
+                    break;
+            }
+        }
 
+        private void AddValueWhenDefined<T>(LineItem lineItem, string key, T value)
+        {
+            if (value == null) return;
+            lineItem.PropertyBag[key] = value;
+        }
     }
 }
 ;
