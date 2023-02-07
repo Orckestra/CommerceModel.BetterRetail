ff968f4d1f49b7a5c3f4767a1c36c4747f905096
Olga Ignatenko <Olga.Ignatenko@orckestra.com>
2023-02-07 12:20:29 +0200

    changes

--- src/CommerceModel.BetterRetail/artifacts/OOE/BetterRetail/CustomExecuteSQLScriptActivity/Foundation/AddTemplateVariables.sql
+++ src/CommerceModel.BetterRetail/artifacts/OOE/BetterRetail/CustomExecuteSQLScriptActivity/Foundation/AddTemplateVariables.sql
@@ -2,26 +2,19 @@
 IF NOT EXISTS (SELECT * FROM [dbo].[TEMPLATEVARIABLE] WHERE [Name] = 'OrderReturnCenterContactPhoneNumber' AND [ScopeName] = 'Global')
 BEGIN
 
-	DECLARE @ContactPhoneNumberTemplateID UNIQUEIDENTIFIER
-	SET @ContactPhoneNumberTemplateID = NEWID()
-	INSERT [dbo].[TEMPLATEVARIABLE] ([TemplateVariable_Id], [Name], [ScopeName], [Description], [Created], [CreatedBy], [LastModified], [LastModifiedBy]) VALUES (@ContactPhoneNumberTemplateID, N'OrderReturnCenterContactPhoneNumber', N'Global', N'Order Return Center Contact Phone Number', GETUTCDATE(), GETUTCDATE(), GETUTCDATE(), NULL)
+	INSERT [dbo].[TEMPLATEVARIABLE] ([TemplateVariable_Id], [Name], [ScopeName], [Description], [Created], [CreatedBy], [LastModified], [LastModifiedBy]) VALUES (N'822a10ff-c4bc-4bad-8f30-268b0f5e65f7', N'OrderReturnCenterContactPhoneNumber', N'Global', N'Order Return Center Contact Phone Number', GETUTCDATE(), GETUTCDATE(), GETUTCDATE(), NULL)
 
-	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (@ContactPhoneNumberTemplateID, N'en-ca', N'11111111')
-	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (@ContactPhoneNumberTemplateID, N'en-us', N'11111111')
-	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (@ContactPhoneNumberTemplateID, N'fr-ca', N'11111111')
+	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (N'822a10ff-c4bc-4bad-8f30-268b0f5e65f7', N'en-ca', N'11111111')
+	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (N'822a10ff-c4bc-4bad-8f30-268b0f5e65f7', N'en-us', N'11111111')
+	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (N'822a10ff-c4bc-4bad-8f30-268b0f5e65f7', N'fr-ca', N'11111111')
 END
 
 IF NOT EXISTS (SELECT * FROM [dbo].[TEMPLATEVARIABLE] WHERE [Name] = 'OrderReturnCenterContactEmail' AND [ScopeName] = 'Global')
 BEGIN
 
-	DECLARE @ContactEmailTemplateID UNIQUEIDENTIFIER
-	SET @ContactEmailTemplateID = NEWID()
+	INSERT [dbo].[TEMPLATEVARIABLE] ([TemplateVariable_Id], [Name], [ScopeName], [Description], [Created], [CreatedBy], [LastModified], [LastModifiedBy]) VALUES (N'667077d9-45e1-47f7-9f12-67a135c40adf', N'OrderReturnCenterContactEmail', N'Global', N'Order Return Center Contact Email', GETUTCDATE(), GETUTCDATE(), GETUTCDATE(), NULL)
 
-	DECLARE @TemplateLocalizeValue AS VARCHAR(100)='example@email.com'
-
-	INSERT [dbo].[TEMPLATEVARIABLE] ([TemplateVariable_Id], [Name], [ScopeName], [Description], [Created], [CreatedBy], [LastModified], [LastModifiedBy]) VALUES (@ContactEmailTemplateID, N'OrderReturnCenterContactEmail', N'Global', N'Order Return Center Contact Email', GETUTCDATE(), GETUTCDATE(), GETUTCDATE(), NULL)
-
-	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (@ContactEmailTemplateID, N'en-ca', @TemplateLocalizeValue)
-	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (@ContactEmailTemplateID, N'en-us', @TemplateLocalizeValue)
-	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (@ContactEmailTemplateID, N'fr-ca', @TemplateLocalizeValue)
+	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (N'667077d9-45e1-47f7-9f12-67a135c40adf', N'en-ca', 'example@@email.com')
+	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (N'667077d9-45e1-47f7-9f12-67a135c40adf', N'en-us', 'example@@email.com')
+	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (N'667077d9-45e1-47f7-9f12-67a135c40adf', N'fr-ca', 'example@@email.com')
 END
