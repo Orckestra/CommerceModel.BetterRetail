1c627f15b6fdd6a7b6f33a18f5134878c3100efc
OlyaAn <59433752+OlyaAn@users.noreply.github.com>
2023-02-08 09:21:17 +0200

    Registered and Guests initiate a return request: Email verification t… (#157)
    * Registered and Guests initiate a return request: Email verification template
    
    * update
    
    * removed not need
    
    * updated display name
    
    ---------
    
    Co-authored-by: Olga Ignatenko <Olga.Ignatenko@orckestra.com>

--- src/CommerceModel.BetterRetail/artifacts/OOE/BetterRetail/CustomExecuteSQLScriptActivity/Foundation/AddTemplateVariables.sql
+++ src/CommerceModel.BetterRetail/artifacts/OOE/BetterRetail/CustomExecuteSQLScriptActivity/Foundation/AddTemplateVariables.sql
@@ -18,3 +18,13 @@ BEGIN
 	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (N'667077d9-45e1-47f7-9f12-67a135c40adf', N'en-us', 'example@@email.com')
 	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (N'667077d9-45e1-47f7-9f12-67a135c40adf', N'fr-ca', 'example@@email.com')
 END
+
+IF NOT EXISTS (SELECT * FROM [dbo].[TEMPLATEVARIABLE] WHERE [Name] = 'OrderReturnCenterBaseUrl' AND [ScopeName] = 'Global')
+BEGIN
+
+	INSERT [dbo].[TEMPLATEVARIABLE] ([TemplateVariable_Id], [Name], [ScopeName], [Description], [Created], [CreatedBy], [LastModified], [LastModifiedBy]) VALUES (N'1eaf3f0d-d006-4653-abde-c24f290a3fb0', N'OrderReturnCenterBaseUrl', N'Global', N'Order Return Center Base Url', GETUTCDATE(), GETUTCDATE(), GETUTCDATE(), NULL)
+
+	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (N'1eaf3f0d-d006-4653-abde-c24f290a3fb0', N'en-ca', '')
+	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (N'1eaf3f0d-d006-4653-abde-c24f290a3fb0', N'en-us', '')
+	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (N'1eaf3f0d-d006-4653-abde-c24f290a3fb0', N'fr-ca', '')
+END
