cabc899d51ce306c98ac9fdf20c53c910a7fe741
joclefrancois <34777072+joclefrancois@users.noreply.github.com>
2025-03-06 10:55:33 -0500

    Fixed template variables registry values (#231)
    Story: AB#81990
    
    Co-authored-by: Jocelyn <jocelyn.lefrancois@orckestra.com>

--- src/CommerceModel.BetterRetail/artifacts/OOE/BetterRetail/CustomExecuteSQLScriptActivity/Foundation/AddTemplateVariables.sql
+++ src/CommerceModel.BetterRetail/artifacts/OOE/BetterRetail/CustomExecuteSQLScriptActivity/Foundation/AddTemplateVariables.sql
@@ -2,7 +2,7 @@
 IF NOT EXISTS (SELECT * FROM [dbo].[TEMPLATEVARIABLE] WHERE [Name] = 'OrderReturnCenterContactPhoneNumber' AND [ScopeName] = 'Global')
 BEGIN
 
-	INSERT [dbo].[TEMPLATEVARIABLE] ([TemplateVariable_Id], [Name], [ScopeName], [Description], [Created], [CreatedBy], [LastModified], [LastModifiedBy]) VALUES (N'822a10ff-c4bc-4bad-8f30-268b0f5e65f7', N'OrderReturnCenterContactPhoneNumber', N'Global', N'Order Return Center Contact Phone Number', GETUTCDATE(), GETUTCDATE(), GETUTCDATE(), NULL)
+	INSERT [dbo].[TEMPLATEVARIABLE] ([TemplateVariable_Id], [Name], [ScopeName], [Description], [Created], [CreatedBy], [LastModified], [LastModifiedBy]) VALUES (N'822a10ff-c4bc-4bad-8f30-268b0f5e65f7', N'OrderReturnCenterContactPhoneNumber', N'Global', N'Order Return Center Contact Phone Number', GETUTCDATE(), 'SYSTEM', GETUTCDATE(), 'SYSTEM')
 
 	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (N'822a10ff-c4bc-4bad-8f30-268b0f5e65f7', N'en-ca', N'11111111')
 	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (N'822a10ff-c4bc-4bad-8f30-268b0f5e65f7', N'en-us', N'11111111')
@@ -12,7 +12,7 @@ END
 IF NOT EXISTS (SELECT * FROM [dbo].[TEMPLATEVARIABLE] WHERE [Name] = 'OrderReturnCenterContactEmail' AND [ScopeName] = 'Global')
 BEGIN
 
-	INSERT [dbo].[TEMPLATEVARIABLE] ([TemplateVariable_Id], [Name], [ScopeName], [Description], [Created], [CreatedBy], [LastModified], [LastModifiedBy]) VALUES (N'667077d9-45e1-47f7-9f12-67a135c40adf', N'OrderReturnCenterContactEmail', N'Global', N'Order Return Center Contact Email', GETUTCDATE(), GETUTCDATE(), GETUTCDATE(), NULL)
+	INSERT [dbo].[TEMPLATEVARIABLE] ([TemplateVariable_Id], [Name], [ScopeName], [Description], [Created], [CreatedBy], [LastModified], [LastModifiedBy]) VALUES (N'667077d9-45e1-47f7-9f12-67a135c40adf', N'OrderReturnCenterContactEmail', N'Global', N'Order Return Center Contact Email', GETUTCDATE(), 'SYSTEM', GETUTCDATE(), 'SYSTEM')
 
 	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (N'667077d9-45e1-47f7-9f12-67a135c40adf', N'en-ca', 'example@@email.com')
 	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (N'667077d9-45e1-47f7-9f12-67a135c40adf', N'en-us', 'example@@email.com')
@@ -22,7 +22,7 @@ END
 IF NOT EXISTS (SELECT * FROM [dbo].[TEMPLATEVARIABLE] WHERE [Name] = 'OrderReturnCenterBaseUrl' AND [ScopeName] = 'Global')
 BEGIN
 
-	INSERT [dbo].[TEMPLATEVARIABLE] ([TemplateVariable_Id], [Name], [ScopeName], [Description], [Created], [CreatedBy], [LastModified], [LastModifiedBy]) VALUES (N'1eaf3f0d-d006-4653-abde-c24f290a3fb0', N'OrderReturnCenterBaseUrl', N'Global', N'Order Return Center Base Url', GETUTCDATE(), GETUTCDATE(), GETUTCDATE(), NULL)
+	INSERT [dbo].[TEMPLATEVARIABLE] ([TemplateVariable_Id], [Name], [ScopeName], [Description], [Created], [CreatedBy], [LastModified], [LastModifiedBy]) VALUES (N'1eaf3f0d-d006-4653-abde-c24f290a3fb0', N'OrderReturnCenterBaseUrl', N'Global', N'Order Return Center Base Url', GETUTCDATE(), 'SYSTEM', GETUTCDATE(), 'SYSTEM')
 
 	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (N'1eaf3f0d-d006-4653-abde-c24f290a3fb0', N'en-ca', '')
 	INSERT [dbo].[TEMPLATEVARIABLE_LOCALIZE] ([TemplateVariable_Id], [CultureIso], [Value]) VALUES (N'1eaf3f0d-d006-4653-abde-c24f290a3fb0', N'en-us', '')
