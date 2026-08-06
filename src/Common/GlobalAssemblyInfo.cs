c76e599099a0704d30c5c4304da2347b56bb631e
orck-adrouin <alexandre.drouin@orckestra.com>
2026-08-06 15:54:08 -0400

    Upgrade to ServiceStack v10 (#265)
    * Upgrade to ServiceStack v10
    
     #84896

--- src/Common/GlobalAssemblyInfo.cs
+++ src/Common/GlobalAssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Orckestra Technologies Inc.")]
 [assembly: AssemblyProduct("Orckestra Commerce")]
-[assembly: AssemblyCopyright("© 2025 Orckestra Technologies Inc. All rights reserved.")]
+[assembly: AssemblyCopyright("© 2026 Orckestra Technologies Inc. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
